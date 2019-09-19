c4e35f77b20986990af31eea43ee951a502492bd
mauORod <43222399+mauORod@users.noreply.github.com>
2019-09-19 11:19:26 -0300

    Projeto Web em .NET
    Porque .NET é melhor

---
+++ NetDataChallenge/NetDataChallenge/Models/PessoaModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDataChallenge.Models
+{
+    public class PessoaModel
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Rg { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Cnpj { get; set; }
+        public string SearchPage { get; set; }
+        public string UidCreated { get; set; }
+
+        public string CreatedAt { get; set; }
+
+        public string PersonId { get; set; }
+
+        public IList<StatusModel> ListaStatus { get; set; }
+
+
+    }
+}
---
+++ NetDataChallenge/NetDataChallenge/Models/StatusModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDataChallenge.Models
+{
+    public class StatusModel
+    {
+        public string Portal { get; set; }
+        public string Status { get; set; }
+    }
+}
