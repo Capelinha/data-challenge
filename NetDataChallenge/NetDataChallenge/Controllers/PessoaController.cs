8aafd25e39cd5480ad99661d1a8215c90b042ab3
Caiuno01 <43186403+Caiuno01@users.noreply.github.com>
2019-09-25 09:41:27 -0300

    Add files via upload

--- NetDataChallenge/NetDataChallenge/Controllers/PessoaController.cs
+++ NetDataChallenge/NetDataChallenge/Controllers/PessoaController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NetDataChallenge.Models;
+using NetDataChallenge.Service;
 
 namespace NetDataChallenge.Controllers
 {
     public class PessoaController : Controller
     {
+
+        private readonly PessoaService pessoaService;
+
+        public PessoaController()
+        {
+            pessoaService = new PessoaService();
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Pessoas = pessoaService.FindAll();
+            return View(new PessoaModel() {
+                    UidCreated = "sfdgsfjksadlghkalhsdkgadçkAKJHDASDKF"
+                });
+        }
+
+        [HttpPost]
+        public IActionResult Search(PessoaModel pessoa)
+        {
+            ViewBag.Pessoas = pessoaService.FindAll();
+            pessoaService.Insert(pessoa);
+            return RedirectToAction("Index", "Pessoa");
         }
     }
 }
