043240b8e6c7c2a33697609a7594e408c212f465
Caiuno01 <43186403+Caiuno01@users.noreply.github.com>
2019-09-25 09:42:07 -0300

    Add files via upload

--- NetDataChallenge/NetDataChallenge/Models/PessoaModel.cs
+++ NetDataChallenge/NetDataChallenge/Models/PessoaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,22 @@ namespace NetDataChallenge.Models
 {
     public class PessoaModel
     {
-
+        [Display(Name = "Nome:")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Sobrenome:")]
         public string LastName { get; set; }
 
+        [Display(Name = "RG:")]
         public string Rg { get; set; }
 
+        [Display(Name = "CPF:")]
         public string Cpf { get; set; }
 
+        [Display(Name = "CNPJ:")]
         public string Cnpj { get; set; }
+
+        [Display(Name = "Portais:")]
         public string SearchPage { get; set; }
         public string UidCreated { get; set; }
 
