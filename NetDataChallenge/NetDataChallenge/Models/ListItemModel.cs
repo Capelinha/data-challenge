20971af03d2ea2f647de4e40de1b9fcfe0c5463e
Caiuno01 <43186403+Caiuno01@users.noreply.github.com>
2019-09-26 08:54:31 -0300

    Add files via upload

---
+++ NetDataChallenge/NetDataChallenge/Models/ListItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDataChallenge.Models
+{
+    public class ListItemModel
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public double Progress { get; set; }
+        public double Correct { get; set; }
+        public bool Status { get; set; }
+        public string id { get; set; }
+    }
+}
